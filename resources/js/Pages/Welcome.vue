0ca09e780534ec892b28cca3908887e8a594d1d2
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-02-25 15:07:32 +0800

    visualize match

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -74,6 +74,13 @@ const match = () => {
     });
 };
 
+const formatNumber = (value) => {
+    return Number(parseFloat(value).toFixed(2)).toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  });
+}
+
 </script>
 
 <template>
@@ -142,7 +149,11 @@ const match = () => {
                             class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                         >
                             <PrimaryButton :disabled="form.processing">Match</PrimaryButton>
-                            {{ loan_data }}
+                            <div v-for="ld in loan_data" :key="ld.sku" class="w-full">
+                                <h1 class="text-base">SKU: <span class="font-bold">{{ ld.sku }}</span></h1>
+                                <p class="text-sm">Name: <span class="font-bold">{{ ld.name }}</span></p>
+                                <p class="text-sm">TCP: <span class="font-bold">₱ {{ formatNumber(ld.total_contract_price) }}</span></p>
+                            </div>
                         </div>
 
                         <div
